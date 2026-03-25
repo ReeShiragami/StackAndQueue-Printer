21b344ace9cbfe5aa3cb25457f366234651e6b1d
ReeShiragami <adamabdigusti@gmail.com>
2026-03-25 10:50:20 +0700

    FINAL VERSION

--- printqueue.c
+++ printqueue.c
@@ -3,7 +3,11 @@
 #include <string.h>
 
 #define MAX 10
-typedef int itemtype; 
+typedef struct{
+    int id;
+    char nama[50]; 
+}itemtype;  
+
 typedef struct {
     itemtype item [MAX]; 
     int front; 
@@ -28,25 +32,105 @@ int kosong (queue *q) {
 void enqueue (queue *q, itemtype x) { 
     if (penuh(q)) {
         printf("Antrian printer penuh!\n");
+        printf("========================================\n");
         return; 
     }
 
     q->rear = (q->rear +1) % MAX; 
     q->item[q->rear] = x; 
     q->count++; 
-    printf ("Dokumen %d telah masuk antrian\n", x);
+    printf ("Dokumen [%d - %s] telah masuk antrian\n", x.id, x.nama);
+    printf("========================================\n");
 }
 
-void dequeue (queue *q) {
+itemtype dequeue (queue *q) {
+    itemtype kosong_item = {-1,""};
+    
     if(kosong(q)) {
         printf("Antrian kosong\n"); 
-        return; 
+        printf("========================================\n");
+        return kosong_item; 
     }
 
     itemtype temp = q->item[q->front]; 
     
     q->front = (q->front + 1) % MAX; 
     q->count--;
-    
     return temp; 
+}
+
+void menu() { 
+    printf("\n=== Menu Antrian Printer ===\n");
+    printf("1. Masukkan Dokumen(Enqueue)\n");
+    printf("2. Mencetak Dokumen(Dequeue)\n");
+    printf("3. Tampilkan Antrian\n");
+    printf("0. Keluar\n");
+    printf("===========================\n");
+}
+
+void tampil(queue *q) {
+    if (kosong(q)) {
+        printf("Antrian kosong!\n");
+        return;
+    }
+    printf("========================================\n");
+    printf("Daftar Antrian Printer:\n");
+    for (int i = 0; i < q->count; i++) {
+        int index = (q->front + i) % MAX;
+
+        printf("%d. ID: %d, Nama: %s\n", i+1, q->item[index].id, q->item[index].nama);
+    }
+    printf("========================================\n");
+}
+
+int main() { 
+    queue q; 
+    inisialisasi(&q);
+    int pilihan; 
+    itemtype dokumen;
+    do { 
+        menu(); 
+        printf("Pilih menu : "); 
+        scanf("%d", &pilihan); 
+        switch (pilihan) {
+            case 1: 
+            printf("========================================\n");
+            
+            printf("Masukkan ID dokumen yang ingin dicetak: ");
+            scanf("%d", &dokumen.id); 
+
+            printf("Masukkan nama dokumen: ");
+            scanf(" %[^\n]", dokumen.nama);
+
+            printf("========================================\n");
+            
+            enqueue(&q, dokumen); 
+            break;
+            
+            case 2: {
+                itemtype cetak = dequeue(&q); 
+                if (cetak.id != -1) {
+                    printf("========================================\n");
+                    printf("Mencetak dokumen : \n");
+                    printf("ID: %d\nNama: %s\n", cetak.id, cetak.nama); 
+                    printf("========================================\n");
+                }
+                break;
+            }
+            case 3: 
+            tampil(&q); 
+            break;
+
+            case 0: 
+            printf("Keluar dari program.\n");
+            printf("========================================\n");
+                break;
+
+            default: 
+                printf("Pilihan tidak valid. Silakan coba lagi.\n");
+                printf("========================================\n");
+        }
+    }
+    while (pilihan != 0); 
+    return 0;
 }
--- printstack.c
+++ printstack.c
@@ -3,7 +3,10 @@
 #include <string.h>
 
 #define MAX 10
-typedef int Itemtype ; //Mendeklarasikan tipe bentukan Stack
+typedef struct{
+   int id; 
+   char nama[50];
+} Itemtype ; //Mendeklarasikan tipe bentukan Stack
 typedef  struct {
    Itemtype item[MAX];
    int count;
@@ -24,22 +27,96 @@ int Kosong (Stack *s){
 void Push (Stack *s, Itemtype x) {
     if (Penuh(s)) {
         printf("Stack penuh! Tidak dapat menambahkan elemen.\n");
-    } else {
+    } 
+    else {
         s->item[s->count] = x; // Menambahkan elemen ke Stack
         s->count++; // Meningkatkan jumlah elemen dalam Stack
+        printf("Dokumen [%d - %s] telah masuk antrian\n", x.id, x.nama);
     }
 }
 
 Itemtype Pop (Stack *s)
 {
+   Itemtype kosong_item = {-1, ""}; //Menyimpan nilai kosong jika Stack kosong
+
    Itemtype temp; //Menyimpan elemen yang akan di-pop
    if(Kosong(s)) {
       printf("Stack Kosong\n");
-      return ' ';// Mengembalikan nilai kosong jika Stack kosong
+      return kosong_item;// Mengembalikan nilai kosong jika Stack kosong
    }
+
    else {
       s->count--;
       temp=s->item[s->count];
       return(temp);// Mengembalikan elemen yang di-pop
    }
+}
+
+void menu() {
+      printf("\n=== Menu Stack ===\n");
+      printf("1. Masukan Dokumen(Push)\n");
+      printf("2. Mencetak Dokumen(Pop)\n");
+      printf("3. Tampilkan Antrian \n");
+      printf("0. Keluar\n");
+      printf("==================\n");
+}
+
+void tampil(Stack *s) {
+   if (Kosong(s)) {
+      printf("Stack kosong");
+   } 
+   else{
+      printf("Isi Stack (Prioritas Atas):\n"); 
+      for (int i = s->count -1; i >= 0; i--) {
+         printf("%d. ID: %d, Nama: %s\n", s->count - i, s->item[i].id, s->item[i].nama);
+      }
+   }
+   printf("========================================\n");
+}
+
+int main () {
+   Stack s; 
+   Inisialisasi(&s); 
+   int pilihan; 
+   Itemtype elemen; 
+   do
+   {
+      menu(); 
+      printf("Pilih menu: ");
+      scanf("%d", &pilihan); 
+      switch (pilihan) {
+         case 1: 
+            printf("========================================\n");
+            printf("Masukkan ID dokumen yang ingin ditambahkan: ");
+            scanf("%d", &elemen.id);
+            printf("Masukkan nama dokumen yang ingin ditambahkan: ");
+            scanf(" %[^\n]", elemen.nama); // Membaca string dengan spasi 
+            Push(&s, elemen); //menambahkan elemen ke stack
+            break;
+         
+         case 2: 
+            {
+               Itemtype hasilPop = Pop(&s); //menghapus elemen dari stack
+               if (hasilPop.id != -1) {
+                  printf("========================================\n");
+                  printf("Mencetak dokumen\n");
+                  printf("ID: %d\nNama: %s\n", hasilPop.id, hasilPop.nama);
+                  printf("========================================\n");
+               }
+               break; 
+            }
+
+         case 3: 
+            tampil(&s); //menampilkan isi stack
+            break; 
+
+         case 0: 
+            printf("Keluar dari program.\n"); 
+            break; 
+            
+         default: 
+            printf("Pilihan tidak valid. Silakan coba lagi.\n");
+      }
+   } while (pilihan !=0 );
+   return 0; 
 }
